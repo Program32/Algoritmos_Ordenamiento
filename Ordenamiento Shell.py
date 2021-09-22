a5f451742485f530fff3feee0fff0965f6761258
Program32 <89926024+Program32@users.noreply.github.com>
2021-09-21 22:28:57 -0500

    Add files via upload
    Proyecto en PHP

---
+++ Ordenamiento Shell.py
@@ -0,0 +1,17 @@
+#algoritmo
+def shell(seq):
+    inc = len(seq) // 2
+    while inc:
+        for i, el in enumerate(seq[inc:], inc):
+            while i >= inc and seq[i - inc] > el:
+                seq[i] = seq[i - inc]
+                i -= inc
+            seq[i] = el
+        inc = 1 if inc == 2 else inc * 5 // 11
+
+data = [22, 7, 2, -5, 8, 4]
+shell(data)
+print(data)
+
+
+
